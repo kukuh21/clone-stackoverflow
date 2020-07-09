399529e599aba6d9d88812df4518b62a541161e8
Kukuh Aprianto <kaprianto@gmail.com>
2020-07-09 18:47:39 +0700

    templateadminlte

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,53 @@
-@extends('layouts.app')
+@extends('layouts.master')
+
+@section('title')
+    <title>Dashboard</title>
+@endsection
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+    <div class="content-wrapper">
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1 class="m-0 text-dark">Dashboard</h1>
+                    </div>
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><a href="">Home</a></li>
+                            <li class="breadcrumb-item active">Dashboard</li>
+                        </ol>
+                    </div>
                 </div>
             </div>
         </div>
+
+        <!-- Main content -->
+        <section class="content" id="dw">
+            <div class="container-fluid">
+                <div class="row">
+
+                  <div class="card col-md-12">
+                    <div class="card-header with-border">
+                        <h3 class="card-title">Selamat Datang, {{ Auth::user()->name }}</h3>
+                    </div>
+                    <div class="card-body">
+
+                    </div>
+                  </div>
+
+                </div>
+            </div>
+        </section>
     </div>
-</div>
+@endsection
+
+@section('js')
+  <script src="{{ asset('plugins/datatables/jquery.dataTables.js') }}"></script>
+  <script src="{{ asset('plugins/datatables/dataTables.bootstrap4.js') }}"></script>
+  <script>
+    $(function () {
+      $("#example1").DataTable();
+    });
+  </script>
 @endsection
---
+++ resources/views/layouts/module/sidebar.blade.php
@@ -0,0 +1,36 @@
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+  <!-- Brand Logo -->
+  <a href="index3.html" class="brand-link">
+      <img src="{{ asset('dist/img/AdminLTELogo.png') }}" alt="Sanber" class="brand-image img-circle elevation-3"
+          style="opacity: .8">
+      <span class="brand-text font-weight-light">SanberCode</span>
+  </a>
+
+  <!-- Sidebar -->
+  <div class="sidebar">
+      <nav class="mt-2">
+          <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+              <li class="nav-item has-treeview menu-open">
+                  <a href="#" class="nav-link active">
+                      <i class="nav-icon fa fa-dashboard"></i>
+                      <p>
+                          Dashboard
+                      </p>
+                  </a>
+              </li>
+
+              <li class="nav-item has-treeview">
+                <a class="nav-link" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                    <i class="nav-icon fa fa-sign-out"></i>
+                    <p>
+                        Logout
+                    </p>
+                </a>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    @csrf
+                </form>
+              </li>
+          </ul>
+      </nav>
+  </div>
+</aside>
